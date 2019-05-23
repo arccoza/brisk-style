bf4edc23a84068c3c395a72e0214e136bc1c9da1
Adrien <adrien@arccoza.com>
2019-05-23 14:48:31 +0900

    Update theme props

--- src/theme.js
+++ src/theme.js
@@ -1,16 +1,21 @@
 'use strict'
 const undefined = void 0
+const Color = require('color')
 
 
 function Theme() {
-  this.accent = Color('rgba(103, 61, 171, 1)')
-  this.accentMuted = this.accent.alpha(0.4)
-  this.bgPrimary = Color('#FFF')
-  this.bgSecondary = this.bgPrimary.darken(0.2)
-  this.lineColor = Color('#CCC')
-  this.lineLight = 0.06
-  this.lineRegular = 0.1
-  this.lineBold = 0.16
+  this.accentA = Color('rgba(103, 61, 171, 1)')
+  this.accentB = this.accentA.alpha(0.67)
+  this.inkA = Color('#444')
+  this.inkB = this.inkA.lighten(0.2)
+  this.canvasA = Color('#FFF')
+  this.canvasB = this.canvasA.darken(0.2)
+  this.textT = 100
+  this.textR = 400
+  this.textH = 600
+  this.strokeS = 0.0625
+  this.strokeM = 0.125
+  this.strokeL = 0.25
 }
 
 
