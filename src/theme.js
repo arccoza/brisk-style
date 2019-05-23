acb11fc79daa2e97779159e82b17c08d9a3caf1c
Adrien <adrien@arccoza.com>
2019-05-23 16:27:19 +0900

    Update theme props

--- src/theme.js
+++ src/theme.js
@@ -4,9 +4,20 @@ const Color = require('color')
 
 
 function Theme() {
+  var accent = Color('rgba(103, 61, 171, 1)'), ink = Color('#444')
+  var canvas = Color('#FFF')
+
+  this.accent = [accent, accent.alpha(0.67), accent.alpha(0.4)]
+  this.ink = [ink, ink.lighten(0.2), ink.lighten(0.4)]
+  this.canvas = [canvas, canvas.darken(0.2), canvas.darken(0.4)]
+  this.text = [100, 400, 600]
+  this.stroke = [0.0625, 0.125, 0.25]
+  this.roundness = [0.5, 1, 100000]
+  this.elevation = [0, 2, 8]  // For box-shadow
+  
   this.accentA = Color('rgba(103, 61, 171, 1)')
   this.accentB = this.accentA.alpha(0.67)
-  this.inkA = Color('#444')
+  this.inkA = Color('#666')
   this.inkB = this.inkA.lighten(0.2)
   this.canvasA = Color('#FFF')
   this.canvasB = this.canvasA.darken(0.2)
@@ -19,4 +30,4 @@ function Theme() {
 }
 
 
-module.exports = {Theme}
+module.exports = Theme
