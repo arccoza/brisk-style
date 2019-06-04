205f8c7914e88eca27142c047726d5b0bbf169b3
Adrien <adrien@arccoza.com>
2019-06-04 20:33:27 +0900

    Update styling, fix color, separate static and dynamic styles

--- src/button.js
+++ src/button.js
@@ -3,26 +3,33 @@ const undefined = void 0
 
 
 function button({accent=false, stroke=false, fill=false, outline=false}={}) {
-  fill = fill ? accent ? this.accent[0] : this.ink[2] : 'none'
-  stroke = stroke ? accent ? this.accent[1] : this.ink[2] : ''
-  stroke = `0 0 0 ${this.stroke[0]}em ${stroke} inset`
+  fill = fill ? accent ? this.accent[0] : this.ink[2] : ''
+  stroke = stroke ? accent ? this.accent[1] : this.ink[1] : ''
+  stroke = `0 0 0 ${this.stroke[0]}rem ${stroke} inset`
   outline = outline ? this.accent[2] : ''
   stroke += outline ? `, 0 0 0 ${this.stroke[2]}em ${outline}` : ''
+  var color = (fill ? this.canvas[0] : accent ? this.accent[0] : this.ink[0]).toString()
 
   var style = {
     position: 'relative',
-    backgroundColor: fill,
     display: 'inline-flex',
     alignItems: 'center',
     justifyContent: 'center',
     textAlign: 'center',
     padding: '0.4em 0.8em',
     borderRadius: '0.4em',
-    color: accent ? this.canvas[0] : this.accent[0],
     fontWeight: this.text[2],
-    boxShadow: stroke,
     transition: 'background-color 0.15s, color 0.15s, fill 0.15s, box-shadow 0.15s',
   }
+
+  var diff = {
+    backgroundColor: fill,
+    color,
+    boxShadow: stroke,
+  }
+
+  Object.assign(style, diff)
+  
 
   return this.css ? this.css(style) : style
 }
