5f6e7cbf220b933c21aaef05a5e2339e0dad40b3
Adrien <adrien@arccoza.com>
2019-06-05 15:15:31 +0900

    Remove old props

--- src/theme.js
+++ src/theme.js
@@ -17,18 +17,6 @@ function Theme({css=null}={}) {
   this.roundness = [0.5, 1, 100000]
   this.elevation = [2, 4, 12]  // For box-shadow
   
-  this.accentA = Color('rgba(103, 61, 171, 1)')
-  this.accentB = this.accentA.alpha(0.67)
-  this.inkA = Color('#666')
-  this.inkB = this.inkA.lighten(0.2)
-  this.canvasA = Color('#FFF')
-  this.canvasB = this.canvasA.darken(0.2)
-  this.textT = 100
-  this.textR = 400
-  this.textH = 600
-  this.strokeS = 0.0625
-  this.strokeM = 0.125
-  this.strokeL = 0.25
 }
 
 
