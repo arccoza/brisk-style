bf9d0acd3f7b4c758231dc9c76f8fd398e712ba5
Adrien <adrien@arccoza.com>
2019-06-10 18:30:27 +0900

    Update props

--- src/theme.js
+++ src/theme.js
@@ -5,19 +5,19 @@ const Color = require('color')
 
 function Theme({css=null}={}) {
   var accent = Color('rgba(103, 61, 171, 1)')
-  var canvas = Color('#FFF'), ink = Color('#999')
+  var canvas = Color('#FFF'), ink = Color('#777')
 
   this.css = css
   
   this.rem = 16
-  this.font = ['Roboto', 'Cuprum', 'RobotoMono']
-  this.accent = [accent, accent.alpha(0.67), accent.alpha(0.4)]
-  this.ink = [ink, ink.lighten(0.2), ink.lighten(0.4)]
-  this.canvas = [canvas, canvas.darken(0.2), canvas.darken(0.4)]
-  this.text = [100, 400, 600]
-  this.stroke = [0.0625, 0.125, 0.25]
-  this.roundness = [0.5, 1, 100000]
-  this.elevation = [2, 4, 12]  // For box-shadow
+  this.font = [null, 'Roboto', 'Cuprum', 'RobotoMono']
+  this.fontWeight = [null, 100, 400, 600]
+  this.accent = [null, accent, accent.alpha(0.67), accent.alpha(0.4)]
+  this.ink = [null, ink, ink.lighten(0.45), ink.lighten(0.95)]
+  this.canvas = [null, canvas, canvas.darken(0.2), canvas.darken(0.4)]
+  this.strokeWeight = [null, 0.0625, 0.125, 0.25]
+  this.roundness = [0, 0.25, 0.5, 100000]
+  this.elevation = [0, 0.125, 0.25, 0.5]  // For box-shadow
   
 }
 
