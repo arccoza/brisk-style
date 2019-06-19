c393b711b5af9a2c5a5f72709b458c6d82557112
Adrien <adrien@arccoza.com>
2019-06-19 16:08:52 +0900

    Update zero values

--- src/theme.js
+++ src/theme.js
@@ -10,12 +10,12 @@ function Theme({css=null}={}) {
   this.css = css
   
   this.rem = 16
-  this.font = [null, 'Roboto', 'Cuprum', 'RobotoMono']
-  this.fontWeight = [null, 100, 400, 600]
+  this.font = ['', 'Roboto', 'Cuprum', 'RobotoMono']
+  this.fontWeight = [0, 100, 400, 600]
   this.accent = [null, accent, accent.alpha(0.67), accent.alpha(0.4)]
   this.ink = [null, ink, ink.lighten(0.45), ink.lighten(0.95)]
   this.canvas = [null, canvas, canvas.darken(0.2), canvas.darken(0.4)]
-  this.strokeWeight = [null, 0.0625, 0.125, 0.25]
+  this.strokeWeight = [0, 0.0625, 0.125, 0.25]
   this.roundness = [0, 0.25, 0.5, 100000]
   this.elevation = [0, 0.125, 0.25, 0.5]  // For box-shadow
   
