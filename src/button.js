f06ee9509274bb25800abdc2758a0acfdaaaa0bc
Adrien <adrien@arccoza.com>
2019-06-19 16:15:14 +0900

    Update to use box general styler

--- src/button.js
+++ src/button.js
@@ -1,19 +1,9 @@
 'use strict'
 const undefined = void 0
+const box = require('./box.js')
 
 
-function button({stroke=0, fill=0, face=0, outline=0, elevation=0, roundness=0}={}) {
-  stroke = stroke ? `0 0 0 ${this.strokeWeight[stroke.weight || 1]}rem ${this.shade(stroke)} inset` : ''
-  outline = outline ? `0 0 0 ${this.strokeWeight[outline.weight || 1]}rem ${this.shade(outline)}` : ''
-  var elevationVal = this.elevation[elevation.value]
-  elevation = elevation ? `0 ${elevationVal}rem ${elevationVal+elevationVal}rem ${this.shade(elevation)}` : ''
-  stroke = [stroke, outline, elevation].reduce((s, v) => (v ? `${s}, ${v}` : s))
-  var borderRadius = `${this.roundness[roundness]}rem`
-  fill = fill ? this.shade(fill) : ''
-  var fontWeight = face && face.weight ? this.fontWeight[face.weight] : null
-  var color = face ? this.shade(face).toString() : ''
-
-
+function button(boxOpts) {
   var style = {
     position: 'relative',
     display: 'inline-flex',
@@ -26,16 +16,8 @@ function button({stroke=0, fill=0, face=0, outline=0, elevation=0, roundness=0}=
     transition: 'background-color 0.15s, color 0.15s, fill 0.15s, box-shadow 0.15s',
   }
 
-  var diff = {
-    backgroundColor: fill.toString(),
-    color,
-    fontWeight,
-    borderRadius,
-    boxShadow: stroke,
-  }
-
+  var diff = box.call(this, boxOpts)
   Object.assign(style, diff)
-  
 
   return this.css ? this.css(style) : style
 }
