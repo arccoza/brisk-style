b9481ea3279650fad468981b9e53f6c9ee8a0471
Adrien <adrien@arccoza.com>
2019-06-28 12:26:42 +0900

    Update rect radius calc for use with theme roundness value Make sure radius is never larger than the weight of the line, so you don't end up with elipse shapes; behaves like html/css.

--- src/meter.js
+++ src/meter.js
@@ -64,7 +64,7 @@ return `
 url('data:image/svg+xml;utf8,
 <svg xmlns="http://www.w3.org/2000/svg" width="100%" height="${weight}rem"
   style="font-size:${rem}px;">
-  <rect x="${x1 * 100}%" width="${(x2 - x1) * 100}%" height="100%" rx="${r * weight / 2}rem" fill="${color}" />
+  <rect x="${x1 * 100}%" width="${(x2 - x1) * 100}%" height="100%" rx="${Math.min(r, weight / 2)}rem" fill="${color}" />
 </svg>
 ')
 `
