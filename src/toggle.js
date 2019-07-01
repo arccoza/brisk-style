2b3e7cff38198a6cb02cc2704a80ae31f7346b17
Adrien <adrien@arccoza.com>
2019-07-01 14:18:47 +0900

    Update toggle styler

--- src/toggle.js
+++ src/toggle.js
@@ -3,12 +3,22 @@ const undefined = void 0
 const meter = require('./meter.js')
 
 
-function toggle(props) {
+function toggle(props={}) {
   var style = meter.call(this, {x1: 0, x2: 0.45, fill:{shade:3, accent:1, weight:5}, 
     face:{shade:1, accent:1, weight:5}, pad: 2, roundness:3})
 
-  style.width = '2.5rem'
-  // style.height = '1.25rem'
+  var more = {
+    width: `${this.strokeWeight[props.fill && props.fill.weight || 5] * 2}rem`,
+    verticalAlign: 'bottom',
+    appearance: 'button',
+    outline: 'none',
+    transition: 'background-position 0.3s',
+    '&:checked': {
+      backgroundPosition: '100% center',
+    }
+  }
+
+  Object.assign(style, more)
   return style
 }
 
