e04413888279459f357e6ea247aed279f5750db3
Adrien <adrien@arccoza.com>
2019-07-01 20:00:06 +0900

    Add assign fn

--- src/util.js
+++ src/util.js
@@ -14,5 +14,14 @@ function dataUrl(data, {type='svg', enc='base64'}={}) {
   return `url('data:image/svg+xml;${enc},${enc == 'base64' ? btoa(data) : data}')`
 }
 
+function assign(to, ...from) {
+  for (var i=0, obj, keys; i < from.length; i++) {
+    obj = from[i] || {}, keys = Object.keys(obj)
+    for (var j=0, k, v; k=keys[j], v=obj[k], j < keys.length; j++)
+      to[k] = v
+  }
+  return to
+}
 
-module.exports = {toCss, dataUrl}
+
+module.exports = {toCss, dataUrl, assign}
